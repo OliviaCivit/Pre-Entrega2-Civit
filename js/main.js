c5f3ab9fd1d603f1c152d42d3d1e2a98e861c11a
Olivia <olicivit@gmail.com>
2023-08-29 10:33:23 -0300

    cambiosfinales

--- js/main.js
+++ js/main.js
@@ -1,73 +1,75 @@
-let carrito = [];
-const precios = {
-  Creatina: 15.955,
-  Proteina: 9.133,
-  PreWork: 11.715,
-  Shaker: 1520
-};
-
-function agregarAlCarrito(suplemento) {
-  carrito.push(suplemento);
-  alert(`"${suplemento}" se agregó al carrito.`);
-}
-
-function mostrarProductos() {
-  let productosEnumerados = "";
-  for (const [producto, precio] of Object.entries(precios)) {
-    productosEnumerados += `${producto}: $${precio}\n`;
-  }
-  return productosEnumerados;
-}
-
-function mostrarCarrito() {
-  if (carrito.length === 0) {
-    alert("El carrito está vacío.");
-  } else {
-    alert(`Contenido del carrito:\n\n${carrito.join("\n")}`);
+class Suplemento {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
   }
 }
 
-function calcularTotal() {
-  let total = 0;
-  for (const item of carrito) {
-    total += precios[item];
+class Carrito {
+  constructor() {
+    this.items = [];
   }
-  alert(`Total de la compra: $${total}`);
+
+  agregarItem(suplemento) {
+    this.items.push(suplemento);
+    alert(`"${suplemento.nombre}" se agregó al carrito.`);
+  }
+
+  calcularTotal() {
+    let total = 0;
+    this.items.forEach(item => {
+      total += item.precio;
+    });
+    alert(`Total de la compra: $${total}`);
+  }
+
+  mostrarContenido() {
+    if (this.items.length === 0) {
+      alert("El carrito está vacío.");
+    } else {
+      const contenidoCarrito = this.items.map(item => item.nombre).join("\n");
+      alert(`Contenido del carrito:\n\n${contenidoCarrito}`);
+    }
+  }
+  
+  buscarItemPorNombre(nombre) {
+    return this.items.find(item => item.nombre === nombre);
+  }
 }
+
+const suplementos = [
+  new Suplemento("Creatina", 15.955),
+  new Suplemento("Proteina", 9.133),
+  new Suplemento("PreWork", 11.715),
+  new Suplemento("Shaker", 1520)
+];
+
+const carrito = new Carrito();
 
 while (true) {
   const opcion = prompt(
     "Bienvenido al carrito de compras de suplementos deportivos.\n" +
     "Productos disponibles:\n" +
-    mostrarProductos() +
+    suplementos.map((producto, index) => `${index + 1}. ${producto.nombre}: $${producto.precio}`).join('\n') +
     "\nSeleccione el número del producto que desea agregar al carrito:\n" +
-    "1. Creatina\n" +
-    "2. Proteina\n" +
-    "3. PreWork\n" +
-    "4. Shaker\n" +
-    "5. Ver contenido del carrito\n" +
-    "6. Calcular total de la compra\n" +
-    "7. Salir\n"
+    (suplementos.length + 1) + ". Ver contenido del carrito\n" +
+    (suplementos.length + 2) + ". Calcular total de la compra\n" +
+    (suplementos.length + 3) + ". Salir\n"
   );
 
-  switch (opcion) {
-    case "1":
-    case "2":
-    case "3":
-    case "4":
-      const productoElegido = Object.keys(precios)[parseInt(opcion) - 1];
-      agregarAlCarrito(productoElegido);
-      break;
-    case "5":
-      mostrarCarrito();
-      break;
-    case "6":
-      calcularTotal();
-      break;
-    case "7":
-      alert("Gracias por utilizar el carrito de compras. ¡Hasta luego!");
-      exit(0);
-    default:
-      alert("Opción no válida. Por favor, selecciona una opción válida.");
+  const opcionNumero = parseInt(opcion);
+  
+  if (opcionNumero === suplementos.length + 1) {
+    carrito.mostrarContenido();
+  } else if (opcionNumero === suplementos.length + 2) {
+    carrito.calcularTotal();
+  } else if (opcionNumero === suplementos.length + 3) {
+    alert("Gracias por utilizar el carrito de compras. ¡Hasta luego!");
+    break;
+  } else if (opcionNumero >= 1 && opcionNumero <= suplementos.length) {
+    const productoElegido = suplementos[opcionNumero - 1];
+    carrito.agregarItem(productoElegido);
+  } else {
+    alert("Opción no válida. Por favor, selecciona una opción válida.");
   }
 }
